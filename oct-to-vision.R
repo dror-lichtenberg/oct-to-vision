b8ff1c189bf2e73dca08adb055ae134e62d1fd3f
dror-lichtenberg <dror.lichtenberg@gmail.com>
2020-10-17 12:24:35 +0300

    1st crack at loading OCT data and joining with the CRM data.

--- oct-to-vision.R
+++ oct-to-vision.R
@@ -14,7 +14,7 @@ if(!require(magrittr)) install.packages("magrittr", repos = "http://cran.us.r-pr
 if(!require(Amelia)) install.packages("Amelia", repos = "http://cran.us.r-project.org", dependencies = TRUE) # missmap
 
 ## 1.b Global settings and helper functions
-getOcularHypertension <- function(strInput, whichEye) {
+getOcularTension <- function(strInput, whichEye) {
   # strInput is in format "RE-[:digit:]{1,2}\nLE-[:digit:]{1,2}"
   # whichEye is either "RE" for right-eye or "LE" for left-eye
   if (is.null(strInput) | strInput == "") 
@@ -26,12 +26,12 @@ getOcularHypertension <- function(strInput, whichEye) {
   }
 }
 
-vGetREHypertension<- function(vStrInput) {
-  unlist(lapply(vStrInput, getOcularHypertension, whichEye="RE"))
+vGetRETension<- function(vStrInput) {
+  unlist(lapply(vStrInput, getOcularTension, whichEye="RE"))
 }
 
-vGetLEHypertension<- function(vStrInput) {
-  unlist(lapply(vStrInput, getOcularHypertension, whichEye="LE"))
+vGetLETension<- function(vStrInput) {
+  unlist(lapply(vStrInput, getOcularTension, whichEye="LE"))
 }
 
 getLastAcuity <- function(strInput, whichEye) {
@@ -65,14 +65,29 @@ if (!file.exists("./figs")){dir.create("./figs")}
 ## 2.a Load <TODO Place data on GitHub and read from it>
 patients_data <- read.csv("https://raw.githubusercontent.com/dror-lichtenberg/oct-to-vision/main/data/patients_data.csv", stringsAsFactors = FALSE)
 patients_data$GENDER <- as.factor(patients_data$GENDER)
+patients_data$DATE_OF_BIRTH <- as.Date(patients_data$DATE_OF_BIRTH,format="%d/%m/%Y")
+
 
 visit_summary_data <- read.csv("https://raw.githubusercontent.com/dror-lichtenberg/oct-to-vision/main/data/visit_summary_data.csv", stringsAsFactors = FALSE)
 visit_summary_data %<>% mutate(REACC = vGetREAcuity(ACUITIES), 
                                LEACC = vGetLEAcuity(ACUITIES),
-                               REHT = vGetREHypertension(PATDATA.F5614), 
-                               LEHT = vGetLEHypertension(PATDATA.F5614))
+                               REHT = vGetRETension(PATDATA.F5614), 
+                               LEHT = vGetLETension(PATDATA.F5614))
 missmap(visit_summary_data[,5:8], col = c("red", "lightgreen"), rank.order = FALSE)
 
-# now need the OCT data ..
+#  OCT data ..
+oct_data <- read.csv("https://raw.githubusercontent.com/dror-lichtenberg/oct-to-vision/main/data/oct_data.csv", stringsAsFactors = FALSE)
+oct_data <- oct_data %>% mutate(GENDER = ifelse(SEX=="M", "Male", "Female")) %>% filter(OBF_ID != "") %>% select(-SEX, -X)
+oct_data$DATE_OF_BIRTH <- as.Date(oct_data$DATE_OF_BIRTH,format="%d/%m/%Y")
+oct_data$GENDER <- as.factor(oct_data$GENDER)
+oct_data$OBF_ID <- trimws(oct_data$OBF_ID)
 
+oct_patients_data <- oct_data %>% 
+                      group_by(OBF_ID) %>% 
+                      mutate(NUMBER_OF_SCANS = n()) %>% 
+                      select(OBF_ID, DATE_OF_BIRTH, GENDER, NUMBER_OF_SCANS) %>% 
+                      ungroup() %>% distinct()
 
+patients_join <- left_join(oct_patients_data, patients_data, by = "OBF_ID", suffix = c("_OCT", "_CRM")) %>%
+                      mutate(SAME_GENDER = (GENDER_OCT == GENDER_CRM), SAME_B_DATE = (DATE_OF_BIRTH_OCT == DATE_OF_BIRTH_CRM))
+data_to_validate <- patients_join %>% filter(!SAME_GENDER | !SAME_B_DATE)
